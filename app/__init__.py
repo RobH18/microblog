f496cf9d33b40fb04afb70b1c7a3a5a5c7fea83e
Robert Hallam <roboffice100@gmail.com>
2018-03-27 23:41:51 +0100

    Error handling capability added/fixes made

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -12,4 +16,32 @@ login = LoginManager(app)
 login.login_view = 'login' # such that force login page is set so protected features cannot be accessed by anons
 # 'login' value is function name for login view
 
-from app import routes, models
+from app import routes, models, errors
+
+if not app.debug: # only when FLASK_DEBUG=0
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR) # only reports error and not warnings/informational/debugging messages
+        app.logger.addHandler(mail_handler)
+    
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10) # last 10 log files as backup
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup') # writes line to signify startup, on production will be used to signify restart
+            
