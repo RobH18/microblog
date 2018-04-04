f984bc0cc4d13f9fd9ef32e734e9126137c95626
Robert Hallam <roboffice100@gmail.com>
2018-04-04 21:09:42 +0100

    Spanish and French usability incorporated

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
@@ -10,6 +10,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 
 app = Flask(__name__) # 'from app import app' this is the second 'app' reference
 app.config.from_object(Config) # updates config from Config object
@@ -18,11 +19,11 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login' # such that force login page is set so protected features cannot be accessed by anons
 # 'login' value is function name for login view
+login.login_message = _l('Please log in to access this page.') # overrides default message which is in English coming from extension itself
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-from app import routes, models, errors
+babel = Babel(app)
 
 if not app.debug: # only when FLASK_DEBUG=0
     if app.config['MAIL_SERVER']:
@@ -51,3 +52,8 @@ if not app.debug: # only when FLASK_DEBUG=0
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup') # writes line to signify startup, on production will be used to signify restart
             
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+from app import routes, models, errors
